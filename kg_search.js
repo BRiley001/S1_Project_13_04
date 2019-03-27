513949d7c48cab1475fc9cf1f8e5b55e09d78f8e
Brenden Riley <briley001@west-mec.org>
2019-03-27 15:21:33 -0700

    JS almost done
    Puzzle now meets the requirements, highlights words, adds them to input box, changes words and highlighting when an answer is correct. When all answers are correct, displays a dialog box.

--- kg_search.js
+++ kg_search.js
@@ -27,8 +27,13 @@
 window.onload = init;
 
 var wordCells;
-var pickedLetters = "";
-
+var pickedInputLetters = "";
+var pickedWord = "";
+var crossoutword;
+var selectedWord = [];
+var numCorrectWords = [];
+var solvedWords = [];
+var solvedPuzzle = [];
 function init() {
       document.getElementById("wordTable").innerHTML = drawWordSearch(letterGrid, wordGrid);
       document.getElementById("wordList").innerHTML = showList(wordArray);
@@ -40,30 +45,64 @@ function init() {
       for (var i = 0; i < wordCells.length; i++) {
             wordCells[i].addEventListener("mouseup", cellEndBackground)
       }
+      document.getElementsByTagName("tbody")[0].addEventListener("mouseenter", function () {
+            document.getElementsByTagName("tbody")[0].style.cursor = "pointer";
+      })
 }
 
 function cellBackground() {
       event.target.style.backgroundColor = "pink";
+      selectedWord.push(event.target);
+      event.target.removeEventListener("mouseenter", cellExtBackground);
       event.preventDefault();
       for (var i = 0; i < wordCells.length; i++) {
             wordCells[i].addEventListener("mouseenter", cellExtBackground)
       };
-      pickedLetters += event.target.innerHTML;
-      document.getElementById("pickedLetters").value = pickedLetters;
+      pickedInputLetters += event.target.innerHTML;
+      document.getElementById("pickedLetters").value = pickedInputLetters;
 }
 
 function cellExtBackground() {
       event.target.style.backgroundColor = "pink";
-      pickedLetters += event.target.innerHTML;
-      document.getElementById("pickedLetters").value = pickedLetters;
+      event.target.removeEventListener("mouseenter", cellExtBackground);
+      pickedInputLetters += event.target.innerHTML;
+      document.getElementById("pickedLetters").value = pickedInputLetters;
+      selectedWord.push(event.target);
 }
 
 function cellEndBackground() {
+      selectedWord.push(event.target);
       for (var i = 0; i < wordCells.length; i++) {
             wordCells[i].removeEventListener("mouseenter", cellExtBackground)
       }
-      pickedLetters = "";
-      document.getElementById("pickedLetters").value = pickedLetters;
+      for (var i = 0; i < wordArray.length; i++) {
+            if (wordArray[i].includes(pickedInputLetters)) {
+                  crossoutword = document.querySelectorAll("figure#wordList ul li")
+                  crossoutword[i].style.textDecoration = "line-through";
+                  for (var j = 0; j < selectedWord.length; j++) {
+                        selectedWord[j].style.backgroundColor = "lightgreen";
+                  }
+                  crossoutword = "";
+                  break;
+            } else {
+                  for (var j = 0; j < selectedWord.length; j++) {
+                        selectedWord[j].style.backgroundColor = "";
+                  }
+            }
+
+      };
+      pickedInputLetters = "";
+      document.getElementById("pickedLetters").value = pickedInputLetters;
+      selectedWord = [];
+      solvedWords = document.querySelectorAll("figure#wordList ul li");
+      for (var a = 0; a < solvedWords.length; a++) {
+            if (solvedWords[a].style.textDecoration == "line-through") {
+                  solvedPuzzle.push(solvedWords[a])
+            }
+      }
+      if (solvedPuzzle.length === 25) {
+            window.alert("Congrats! You completed the puzzle")
+      }
 }
 
 
